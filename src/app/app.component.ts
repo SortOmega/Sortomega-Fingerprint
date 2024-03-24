489eb9b9064c6f86ee2c852efe07e4192a581a2b
SortOmega <sortocarlo755@gmail.com>
2024-03-24 11:24:18 -0600

    build: :building_construction: Configuración Base en Angular para habilitar el lector de huellas de Digital Persona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
+import {
+  FingerprintReader,
+  SampleFormat,
+  DeviceConnected,
+  DeviceDisconnected,
+  SamplesAcquired,
+  AcquisitionStarted,
+  AcquisitionStopped,
+} from '@digitalpersona/devices';
+
+import './core/modules/WebSdk';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Sortomega-Fingerprint';
